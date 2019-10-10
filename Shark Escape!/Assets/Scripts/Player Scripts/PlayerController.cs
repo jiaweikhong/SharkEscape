aa04c678c28470fd4c7c459b75ba205995b3a257
Khong Jia Wei <khongjw@gmail.com>
2019-10-10 21:34:07 +0800

    created sprite

--- Shark Escape!/Assets/Scripts/Player Scripts/PlayerController.cs
+++ Shark Escape!/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
     public float speed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    void MoveSubmarine() {
+
+    }
+
 }
