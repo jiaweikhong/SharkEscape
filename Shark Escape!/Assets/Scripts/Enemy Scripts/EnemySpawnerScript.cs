430851477723ac3138d46990f0fea65b77fc58db
Chong Yi Cheng | kunstmatrix <chong.yi.cheng@kunstmatrix.com>
2019-10-11 14:27:34 +0800

    -Completed map scrolling

---
+++ Shark Escape!/Assets/Scripts/Enemy Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+	public float min_X = -6.0f, max_X = 6.0f;
+
+	public GameObject[] enemy_Prefabs;
+
+	public float timer = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void SpawnEnemies(){
+
+    	float pos_X = Random.Range(min_X,max_X);
+    	Vector3 temp = transform.position;
+    	temp.x = pos_X;
+		Instantiate(enemy_Prefabs[Random.Range(0, 4)],temp, Quaternion.identity);
+    	Invoke("SpawnEnemies", timer);
+    }
+}
---
+++ Shark Escape!/Assets/Scripts/Map Scripts/BGScroll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+	public float scroll_Speed = 0.1f;
+
+	private MeshRenderer mesh_Renderer;
+
+	private float y_Scroll;
+	void Awake() {
+		mesh_Renderer = GetComponent<MeshRenderer>();
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scroll();
+    }
+
+    void Scroll(){
+    	y_Scroll = -(Time.time * scroll_Speed);
+
+    	Vector2 offset = new Vector2(0f,y_Scroll);
+    	//_MainTex is main texture attached on our object. In this case, the BG
+    	mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex",offset);
+    }
+}
