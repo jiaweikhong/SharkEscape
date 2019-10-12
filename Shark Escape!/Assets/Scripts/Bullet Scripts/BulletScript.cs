789abc2d6edd805c7e9cbb630f0dcea27600b956
Khong Jia Wei <khongjw@gmail.com>
2019-10-12 15:53:25 +0800

    sub can move

---
+++ Shark Escape!/Assets/Scripts/Bullet Scripts/BulletScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float speed = 5f;
+    public float deactivate_Timer = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();   
+    }
+
+    void Move()
+    {
+        Vector3 temp = transform.position;
+        temp.y += speed * Time.deltaTime;
+        transform.position = temp;
+    }
+
+}
+
+
+
+
+
--- Shark Escape!/Assets/Scripts/Player Scripts/PlayerController.cs
+++ Shark Escape!/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 5f;    // f for float
+    public float min_Y, max_Y, min_X, max_X;
+
+    [SerializeField]
+    private GameObject player_Torpedo;
+
+    [SerializeField]
+    private Transform attack_Point;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +22,50 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        MoveSubmarine();
     }
-    void MoveSubmarine() {
-
+    void MoveSubmarine()
+    {
+        if (Input.GetAxisRaw("Vertical") > 0f)
+        {
+            Vector3 temp = transform.position;
+            temp.y += speed * Time.deltaTime;
+            if (temp.y > max_Y)
+            {
+                temp.y = max_Y;
+            }
+            transform.position = temp;
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0f)
+        {
+            Vector3 temp = transform.position;
+            temp.y -= speed * Time.deltaTime;
+            if (temp.y < min_Y)
+            {
+                temp.y = min_Y;
+            }
+            transform.position = temp;
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            Vector3 temp = transform.position;
+            temp.x += speed * Time.deltaTime;
+            if (temp.x > max_X)
+            {
+                temp.x = max_X;
+            }
+            transform.position = temp;
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            Vector3 temp = transform.position;
+            temp.x -= speed * Time.deltaTime;
+            if (temp.x < min_X)
+            {
+                temp.x = min_X;
+            }
+            transform.position = temp;
+        }
     }
 
 }
