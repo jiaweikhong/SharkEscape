76db5d5647b2d3b06a15905d6796615fe2008821
Khong Jia Wei <khongjw@gmail.com>
2019-10-13 13:15:04 +0800

    player can shoot omnidirectional

--- Shark Escape!/Assets/Scripts/Bullet Scripts/BulletScript.cs
+++ Shark Escape!/Assets/Scripts/Bullet Scripts/BulletScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +8,16 @@ public class BulletScript : MonoBehaviour
     public float speed = 5f;
     public float deactivate_Timer = 3f;
 
+    private float torpedo_X;    // decided by PlayerController.cs
+    private float torpedo_Y;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        torpedo_X = PlayerController.torpedo_X;
+        torpedo_Y = PlayerController.torpedo_Y;
+        SetAngle(torpedo_X, torpedo_Y);     // set the angle of the torpedo flying
+        Invoke("DeactivateGameObject", deactivate_Timer);   // torpedo disappears after deactivate_Timer is up
     }
 
     // Update is called once per frame
@@ -22,10 +29,33 @@ public class BulletScript : MonoBehaviour
     void Move()
     {
         Vector3 temp = transform.position;
-        temp.y += speed * Time.deltaTime;
+        temp.x += speed * Time.deltaTime * torpedo_X;
+        temp.y += speed * Time.deltaTime * torpedo_Y;
         transform.position = temp;
-    }
+    }   // move
 
+    void DeactivateGameObject() {
+        gameObject.SetActive(false);
+    }   // deactivate
+
+    void SetAngle(float x, float y) {
+        if (x >= 0 & y >= 0) {
+            float val = 0 + 90 * -x;
+            transform.rotation = Quaternion.Euler(0, 0, val);   // set rotation
+        }
+        else if (x <= 0 & y >= 0) {
+            float val = 270 + 90 * -y;
+            transform.rotation = Quaternion.Euler(0, 0, val);
+        }
+        else if (x <= 0 & y <= 0) {
+            float val = 90 + 90 * -y;
+            transform.rotation = Quaternion.Euler(0, 0, val);
+        }
+        else if (x >= 0 & y <= 0) {
+            float val= 180 + 90 * x;
+            transform.rotation = Quaternion.Euler(0, 0, val);
+        }
+    }   // set angle
 }
 
 
--- Shark Escape!/Assets/Scripts/Player Scripts/PlayerController.cs
+++ Shark Escape!/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -13,17 +13,28 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private Transform attack_Point;
 
+    public float attack_Timer = 0.35f;
+    private float current_Attack_Timer;
+    private bool canAttack;
+
+    public static float torpedo_X = 0f;
+    public static float torpedo_Y = 1f;
+    public float torpedo_turn_rate;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        current_Attack_Timer = attack_Timer;
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveSubmarine();
+        MoveTorpedoAngle();
+        Attack();
     }
+
     void MoveSubmarine()
     {
         if (Input.GetAxisRaw("Vertical") > 0f)
@@ -66,6 +77,63 @@ public class PlayerController : MonoBehaviour
             }
             transform.position = temp;
         }
-    }
+    }   // move submarine
 
-}
+    void Attack() {
+        
+        attack_Timer += Time.deltaTime;
+        if (attack_Timer > current_Attack_Timer) {
+            canAttack = true;
+        }
+
+        // GetKey is for holding down button. GetKeyDown is for pressing once.
+        if (Input.GetKey(KeyCode.W)) {
+            if (canAttack) {
+                canAttack = false;
+                attack_Timer = 0f;
+                Instantiate (player_Torpedo, attack_Point.position, Quaternion.identity);
+                // play the sound FX
+            }
+        }
+    }   // attack
+
+    void MoveTorpedoAngle() {
+        if (Input.GetKey(KeyCode.A)) {
+            if (torpedo_X >= 0 & torpedo_Y >= 0) {
+                torpedo_X -= torpedo_turn_rate;
+                torpedo_Y += torpedo_turn_rate;
+            }
+            else if (torpedo_X <= 0 & torpedo_Y >= 0) {
+                torpedo_X -= torpedo_turn_rate;
+                torpedo_Y -= torpedo_turn_rate;
+            }
+            else if (torpedo_X <= 0 & torpedo_Y <= 0) {
+                torpedo_X += torpedo_turn_rate;
+                torpedo_Y -= torpedo_turn_rate;
+            }
+            else if (torpedo_X >= 0 & torpedo_Y <= 0) {
+                torpedo_X += torpedo_turn_rate;
+                torpedo_Y += torpedo_turn_rate;
+            }
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            if (torpedo_X >= 0 & torpedo_Y >= 0) {
+                torpedo_X += torpedo_turn_rate;
+                torpedo_Y -= torpedo_turn_rate;
+            }
+            else if (torpedo_X <= 0 & torpedo_Y >= 0) {
+                torpedo_X += torpedo_turn_rate;
+                torpedo_Y += torpedo_turn_rate;
+            }
+            else if (torpedo_X <= 0 & torpedo_Y <= 0) {
+                torpedo_X -= torpedo_turn_rate;
+                torpedo_Y += torpedo_turn_rate;
+            }
+            else if (torpedo_X >= 0 & torpedo_Y <= 0) {
+                torpedo_X -= torpedo_turn_rate;
+                torpedo_Y -= torpedo_turn_rate;
+            }
+        }
+    }   // move torpedo angle
+
+}   // class
